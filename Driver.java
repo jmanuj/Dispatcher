b8b52adc13a94845252f384778e169ddad47701a
Manuj <manujbhalla007@gmail.com>
2013-02-12 13:44:59 +0530

    commit

--- Driver.java
+++ Driver.java
@@ -8,20 +8,25 @@ public class Driver {
 	public static void main(String[] args) {
 		IDispatcher dispatcher = new HashMapDispatcher(3);
 		
-		String pred1 = "(c1 == \"in\") && (c3 == \"wsj.com\")";
-		String pred2 = "(c1 == \"in\") && (c3 == \"wsjh.com\" || c3 == \"nytimes.com\")";
-		String pred3 = "(c1 == \"in\") && (c2 == \"home.php\" || c2 ==\"news.php\") && (c3 == \"huffpost.com\")";
+		String pred = "(c1 == \"in\") && (c2 == \"home.php\" || c2 ==\"news.php\") && (c3 == \"huffpost.com\")";
 		
 		try {
-			dispatcher.registerSubscriber("123", pred1);
-			dispatcher.registerSubscriber("234", pred2);
-			dispatcher.registerSubscriber("234", pred3);
+			
+			for(int i = 0; i < 9000; i++) {
+				dispatcher.registerSubscriber(String.valueOf(i), pred);
+			}
 			
 			String[] event = {"in", "blah", "wsj.com"};	//match
 			String[] event1 = {"hk", "news.php"};		//no match
 			
-			System.out.println(dispatcher.findMatchingIds(event));
-			System.out.println(dispatcher.findMatchingIds(event1));
+			long start = System.currentTimeMillis();
+			//for(long i = 0; i < 99999999; i++) {
+				dispatcher.findMatchingIds(event);
+				dispatcher.findMatchingIds(event1);
+			//}
+			long end = System.currentTimeMillis();
+			
+			System.out.println(end - start);
 			
 		} catch(Exception e) {
 			System.out.println("ex: " + e.getMessage());
